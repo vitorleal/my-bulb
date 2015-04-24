0a50d89486132de6eef98c45434ce53878f52694
Vitor Leal <vitorleal1@gmail.com>
2015-04-24 13:43:56 -0300

    add comments

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
 var restify = require('restify'),
     lifx    = require('lifx'),
     lx      = lifx.init(),
-    server  = restify.createServer();
+    server  = restify.createServer(),
+    bulbStatus = false;
 
+// Turn on the light
 server.get('/on', function(req, res, next) {
   lx.lightsOn();
   // lx.lightsColour(hue, saturation, luminance, whiteColour, fadeTime);
   lx.lightsColour(0x0000, 0x0000, 0x8000, 0x0af0, 0x0513);
+  bulbStatus = true;
 
-  res.send(true);
+  res.send({ on: bulbStatus });
   next();
 });
 
+// Turn off the light
 server.get('/off', function(req, res, next) {
   lx.lightsOff();
+  bulbStatus = false;
 
-  res.send(false);
+  res.send({ on: bulbStatus });
   next();
 });
 
+// Blink the light
 server.get('/blink', function(req, res, next) {
   lx.lightsOff();
 
@@ -46,22 +52,32 @@ server.get('/blink', function(req, res, next) {
     lx.lightsOff();
   }, 6000);
 
-  res.send(false);
+  bulbStatus = false;
+
+  res.send({ blink: true });
   next();
 });
 
+
 console.log('Waiting for gateway...');
 
-lx.on('gateway', function(g) {
-  console.log('New gateway found: ' + g.ip);
+// Find Lifx gateway
+lx.on('gateway', function (gateway) {
+  console.log('New gateway found: ' + gateway.ip);
 
   lifx.setDebug(false);
 
-  server.listen(80, function() {
+  // Start server
+  server.listen(8000, function () {
     console.log('Listening at %s', server.url);
   });
 });
 
-lx.on('bulb', function(b) {
-  console.log('New bulb found: ' + b.name);
+// Log the lifx bulb
+lx.on('bulb', function (bulb) {
+  console.log('New bulb found: ' + bulb.name);
+});
+
+server.on('error', function (error) {
+  console.log('Error: %s', error)
 });
